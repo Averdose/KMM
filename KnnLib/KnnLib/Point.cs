0f14a37f662a44dd930a57b9c6d8ade880e22d21
Kornel Żaba <kornelzaba@gmail.com>
2018-03-07 21:25:57 +0100

    Created Knn lib project, added representation of data

---
+++ KnnLib/KnnLib/Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnnLib
+{
+    //jak czegoś ci brakuje to dodaj
+    public class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        /// <summary>
+        /// Distance to current test sample
+        /// </summary>
+        public double DistanceToSample { get; set; }
+        /// <summary>
+        /// Distance to centre of given class
+        /// </summary>
+        public double DistanceToMean { get; set; }
+        /// <summary>
+        /// True class of point read from input file
+        /// </summary>
+        public string TrueLabel { get; set; }
+        /// <summary>
+        /// Output class given by the algorithm
+        /// </summary>
+        public string ResultLabel { get; set; }
+    }
+}
